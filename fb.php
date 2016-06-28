cd04df9c18512f6df630a3d5839254e1bffaa5a0
quykrb159 <nguyenngocquy020196@gmail.com>
2016-06-28 21:14:58 +0700

    test comment

---
+++ fb.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Khách Sạn VQ</title>
+<link href="<?php echo $plink;?>img/favicon.ico" rel="shortcut icon">
+<link href="css/datepicker3.css" rel="stylesheet">
+<link href="css/styles.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+	<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.6&appId=1451183601574358";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+<div class="fb-comments" data-href="http://localhost/hotelDA" data-width="comments" data-numposts="5"></div>
+
+
+<div class="fb-comments" data-href="http://localhost/hotelDA/ss" data-width="comments" data-numposts="5"></div>
+
+</body>
+</html>
