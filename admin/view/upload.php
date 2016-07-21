6dc476650db80bdb3fccae775dc8b8231f3200bd
quykrb159 <nguyenngocquy020196@gmail.com>
2016-07-22 00:59:16 +0700

    delete

--- admin/view/upload.php
+++
@@ -1,7 +0,0 @@
-<form id="form_upload_ajax" method="post" action="upload.php" enctype="multipart/form-data">
-    <input type="file" name="img[]" multiple/>
-    <input type="submit" value="Upload" id="upload"/>
-  </form>
-  
-  <div id="result">
-  </div>
