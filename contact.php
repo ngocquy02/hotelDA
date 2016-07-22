5d2cba8454361aef8d8907b7b7afb2b20afcd3d0
quykrb159 <nguyenngocquy020196@gmail.com>
2016-07-22 21:13:09 +0700

    CONTACT

---
+++ contact.php
@@ -0,0 +1,53 @@
+<?php
+	require_once('admin/include/config.php');
+	require_once('admin/include/get_list.php');
+	require_once('admin/include/customer_db.php');
+	require_once('admin/include/room_db.php');
+	require_once('admin/link.php');
+	session_start();
+	if (isset($_SESSION['email'])==NULL || isset($_POST['type_room'])==NULL) {
+		header ("Location: ");
+	}
+	$rooms=get_list_room();
+?>  
+<!DOCTYPE html>
+<html>
+<?php include('include/head.php'); ?>
+<body>
+<?php
+	include('include/menu.php');
+?>
+<div class="container wrapper-body" style=" width : 100% ; margin: auto;" >
+	<div class="row" style="width:90%; margin: auto;">
+	<h2 class="text-success list_room_title" style="text-align:center; margin:20px 0px;">LIÊN HỆ</h2>
+	<div class="col-md-12" style="padding: 0px; margin:0px;">
+        <div class="form-group">
+			<label for="1">Tên</label>
+			<input type="text" class="form-control" id="1" name="name" required>
+        </div>
+        
+        <div class="form-group">
+			<label for="2">Điện thoại</label>
+			<input type="tel" class="form-control" id="2" required name="phone">
+        </div>
+        <div class="form-group">
+			<label for="3">Email</label>
+			<input type="email" class="form-control" id="3" required name="email">
+        </div>
+        <div class="form-group">
+			<label for="4">Nội dung</label>
+			<textarea class="form-control" id="4" required name="email" style="height:150px;"></textarea>
+        </div>
+       
+		<button id="button" onclick="return true;"  type="submit" class="btn btn-primary">Gửi
+		</button>
+	</div>								
+	<iframe class="col-md-12" src="https://www.google.com/maps/d/embed?mid=1-KjZNYslWab9NDLm9fhR00hvwkw" style="height:420px;margin:30px;"></iframe>	
+	</div>
+</div>
+	<?php include('include/footer.php'); ?>
+</body>
+</html>
+
+
+
